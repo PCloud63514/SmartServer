314ea6bd87314539d0418f14b9ff89b55f687150
PCloud63514 <pcloud63514@gmail.com>
2022-02-05 14:56:08 +0900

    app-demo app-test 서버 추가

---
+++ applications/app-test/src/main/java/org/macchiato/apptest/AppTestApplication.java
@@ -0,0 +1,11 @@
+package org.macchiato.apptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppTestApplication.class, args);
+    }
+}
