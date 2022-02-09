709f20139af8f96d61728233330b1fe79f00c0a8
PCloud63514 <pcloud63514@gmail.com>
2022-02-10 00:57:26 +0900

    Update

--- components/library-department/src/main/java/org/macchiato/department/library/config/FeignClientConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.macchiato.department.library.config;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@EnableFeignClients
-@Configuration
-public class FeignClientConfiguration {
-}
--- components/library-department/src/main/java/org/macchiato/department/library/network/DepartmentClient.java
+++ components/library-department/src/main/java/org/macchiato/department/library/network/DepartmentClient.java
@@ -1,9 +1,11 @@
 package org.macchiato.department.library.network;
 
+import feign.Headers;
 import org.macchiato.department.library.body.request.AddDepartmentRequest;
 import org.macchiato.department.library.body.request.UpdateDepartmentRequest;
 import org.macchiato.department.library.body.response.DepartmentResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,16 +13,20 @@ import java.util.UUID;
 
 @FeignClient(value = "department", url = "localhost:8080/department")
 public interface DepartmentClient {
-    @PostMapping
-    UUID addDepartment(AddDepartmentRequest request) throws Exception;
-
-    @GetMapping("list")
-    List<DepartmentResponse> getDepartments() throws Exception;
 
     @GetMapping("{departmentId}")
     DepartmentResponse getDepartment(@PathVariable(name = "departmentId") UUID departmentId) throws Exception;
 
-    @PatchMapping({"{departmentId}"})
+    @GetMapping("list")
+    List<DepartmentResponse> getDepartments() throws Exception;
+
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
+    UUID addDepartment(@RequestBody AddDepartmentRequest request) throws Exception;
+
+//    @Headers("Content-Type: application/json")
+    @PatchMapping(value = {"{departmentId}"},
+        consumes = {MediaType.APPLICATION_JSON_VALUE},
+        produces = {MediaType.APPLICATION_JSON_VALUE})
     void updateDepartment(@PathVariable(name = "departmentId") UUID departmentId, @RequestBody UpdateDepartmentRequest request) throws Exception;
 
     @DeleteMapping("{departmentId}")
