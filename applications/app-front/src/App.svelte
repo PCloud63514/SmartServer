c7e07bd3bed6d29eff6d33f13af95ff41b862fc7
PCloud <pcloud63514@gmail.com>
2022-02-10 15:25:10 +0900

    Test

--- applications/app-front/src/App.svelte
+++ applications/app-front/src/App.svelte
@@ -1,21 +1,48 @@
 <script>
+	import {onMount} from "svelte";
+	let departments = []
 	export let name;
+	let departmentName;
+	let description
 	let a = 1;
 	let b = 2;
+	async function getDepartments() {
+		let fetchData = []
+		const result = await fetch("/department/list")
+		const json = await result.json()
+		fetchData.push(...json);
+		departments = [...fetchData]
+	}
+
+	async function addDepartment() {
+		const result = await fetch("http://localhost:8180/department",
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						name: departmentName,
+						description: description
+					})
+				})
+	}
+
+	onMount(async()=> {
+		await getDepartments()
+	})
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 
-	<input type="number" bind:value={a}>
-	<input type="number" bind:value={b}>
+	<input type="text" placeholder="부서 명" bind:value={departmentName}>
+	<input type="text" placeholder="부서 설명" bind:value={description}>
 
-	<p>{a} + {b} = {a + b}</p>
-	<p>{a} - {b} = {a - b}</p>
-	<p>{a} * {b} = {a * b}</p>
-	<p>{a} / {b} = {a / b}</p>
-	<p>{a} % {b} = {a % b}</p>
+
+	<p>{departments}</p>
 </main>
 
 <style>
