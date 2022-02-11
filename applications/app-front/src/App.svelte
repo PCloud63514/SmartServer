74a7e5788b0f0be461e5b0c355bf40df6f45539d
PCloud <pcloud63514@gmail.com>
2022-02-11 16:52:05 +0900

    스벨트 탐방 끝. React나 Vue 쓰는게 맞을듯

--- applications/app-front/src/App.svelte
+++ applications/app-front/src/App.svelte
@@ -1,7 +1,9 @@
 
-<script>
+<script lang="ts">
 	import SvelteTable from "svelte-table";
-	import {onMount} from "svelte"
+	import { Box } from "./components/atoms";
+    import { CrossBox } from "./components/molecules";
+    import {onMount} from "svelte"
 	var departments = [{departmentName: "야!", description: "스벨트 소리!"}]
     let departmentName
     let description
@@ -54,8 +56,18 @@
 	];
 
 </script>
+<div class="header">
+    <CrossBox></CrossBox>
+</div>
 <input type="text" placeholder="부서 명" bind:value={departmentName}>
 <input type="text" placeholder="부서 설명" bind:value={description}>
 <button on:click={addDepartment}>추가</button>
 <SvelteTable columns="{columns}" rows="{departments}"></SvelteTable>
 
+
+<style>
+    .header {
+        width: 500px;
+        border: 1px solid #aaa;
+    }
+</style>
