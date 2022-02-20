7a748d20da2ab7e385e6e9e44f4650dafa80e30a
PCloud63514 <pcloud63514@gmail.com>
2022-02-20 17:26:15 +0900

    Department 수정

--- applications/app-department/src/main/java/org/macchiato/department/AppDepartmentApplication.java
+++ applications/app-department/src/main/java/org/macchiato/department/AppDepartmentApplication.java
@@ -3,7 +3,6 @@ package org.macchiato.department;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @ServletComponentScan
 @SpringBootApplication
